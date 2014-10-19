c488af2c98d06c550a8d2cc8a278288fe9293b43
MO. H <msaib_prog@yahoo.com>
2014-10-19 13:16:34 -0600

    Added validations

--- app/models/restaurent.rb
+++ app/models/restaurent.rb
@@ -1,4 +1,12 @@
 class Restaurent < ActiveRecord::Base
   mount_uploader :image, ImageUploader
   has_many :reviews
+  validates :name, :image, presence: true
+  validates :phone, format: {
+    with: /\A\(\d{3}\) \d{3}- \d{4}\z/,
+    message: "must be in the format (123) 456- 7890"}    
+  validates :website, format: { with: /\Ahttps?:\/\/.*\z/,
+    message: "must start with http:// or https://" }
+  validates :address, format: { with: /\A\d+[^,]+,[^,]+, [A-Z]{2} \d{5}\z/,
+      message: "must be in the format 2355 St-marc, Montreal, QC 10118" }
 end
